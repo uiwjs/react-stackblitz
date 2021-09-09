f633c50a5be0d112af985f92653becab52d31b80
jaywcjlove <398188662@qq.com>
2021-09-09 16:40:41 +0800

    chore(deps): Update dependency kkt to v6.11.0

--- .kktrc.ts
+++ .kktrc.ts
@@ -1,23 +1,28 @@
 import path from 'path';
-import webpack, {Configuration} from 'webpack';
+import webpack, { Configuration } from 'webpack';
 import { LoaderConfOptions } from 'kkt';
 import lessModules from '@kkt/less-modules';
 import rawModules from '@kkt/raw-modules';
 import scopePluginOptions from '@kkt/scope-plugin-options';
 import pkg from './package.json';
 
-export default (conf: Configuration, env: string, options: LoaderConfOptions) => {
+export default (conf: Configuration, env: 'production' | 'development', options: LoaderConfOptions) => {
+  conf = lessModules(conf, env, options);
   conf = rawModules(conf, env, { ...options });
   conf = scopePluginOptions(conf, env, {
     ...options,
     allowedFiles: [
-      path.resolve(process.cwd(), 'README.md')
+      path.resolve(process.cwd(), 'README.md'),
+      path.resolve(process.cwd(), 'src'),
     ]
   });
-  conf = lessModules(conf, env, options);
+  // conf.resolve!.alias = { '@uiw/react-monacoeditor': process.cwd() };
   // Get the project version.
   conf.plugins!.push(new webpack.DefinePlugin({
     VERSION: JSON.stringify(pkg.version),
   }));
+  if (env === 'production') {
+    conf.output = { ...conf.output, publicPath: './' };
+  }
   return conf;
 }
