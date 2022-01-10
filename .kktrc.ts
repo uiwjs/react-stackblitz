b14314f8acb1a0f3320158b01f473bf6cf913488
jaywcjlove <398188662@qq.com>
2022-01-10 17:02:14 +0800

    chore(deps): update dependency kkt to v7.
    Upgrade react-scripts to v5, Support Webpack 5
    https://github.com/kktjs/kkt/issues/198

--- .kktrc.ts
+++ .kktrc.ts
@@ -16,13 +16,39 @@ export default (conf: Configuration, env: 'production' | 'development', options:
       path.resolve(process.cwd(), 'src'),
     ]
   });
-  // conf.resolve!.alias = { '@uiw/react-monacoeditor': process.cwd() };
   // Get the project version.
   conf.plugins!.push(new webpack.DefinePlugin({
     VERSION: JSON.stringify(pkg.version),
   }));
   if (env === 'production') {
     conf.output = { ...conf.output, publicPath: './' };
+    conf.optimization = {
+      ...conf.optimization,
+      splitChunks: {
+        cacheGroups: {
+          reactvendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendor',
+            chunks: 'all',
+          },
+          refractor: {
+            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+            name: 'refractor-prismjs-vendor',
+            chunks: 'all',
+          },
+          runtime: {
+            test: /[\\/]node_modules[\\/](@babel)[\\/]/,
+            name: 'babel-vendor',
+            chunks: 'all',
+          },
+          parse5: {
+            test: /[\\/]node_modules[\\/](parse5)[\\/]/,
+            name: 'parse5-vendor',
+            chunks: 'all',
+          },
+        }
+      }
+    }
   }
   return conf;
 }
